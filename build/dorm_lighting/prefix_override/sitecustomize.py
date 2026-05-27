7cb30a0aedb83ff9cd324e33804773d4d06bcf48
woodenclock <luckyisland3710@gmail.com>
2026-05-27 15:10:42 +0900

    Remove generated ROS build artifacts

--- build/dorm_lighting/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/elvis/projects/smart_home_take_home_assignment/install/dorm_lighting'
